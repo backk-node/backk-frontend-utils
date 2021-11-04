abeda7d06a26585894d0d492f5e7103b29499887
Petri Silen <petri.silen@silensoft.com>
2021-11-04 15:47:31 +0200

    Modified

--- src/getJwtFromSessionStorage.ts
+++ src/getJwtFromSessionStorage.ts
@@ -2,5 +2,7 @@ import AES from 'crypto-js/aes';
 
 export default function getJwtFromSessionStorage(jwtStorageEncryptionKey: string): string | undefined {
   const encryptedJwt = sessionStorage.getItem('jwt');
-  return encryptedJwt ? AES.decrypt(encryptedJwt, jwtStorageEncryptionKey).toString() : undefined;
+  return encryptedJwt
+    ? AES.decrypt(encryptedJwt, jwtStorageEncryptionKey).toString(CryptoJS.enc.Utf8)
+    : undefined;
 }
