8175bc6e10fdb48f4fb70c1c838375833265cc90
Petri Silen <petri.silen@silensoft.com>
2021-11-05 10:32:57 +0200

    Modified

--- src/validation/convertPlainToInstance.ts
+++ src/validation/convertPlainToInstance.ts
@@ -1,5 +1,5 @@
 import { plainToClass } from 'class-transformer';
 
-export default function convertPlainToInstance(plainObject: any, Class: new () => any) {
+export default function convertPlainToInstance<T>(plainObject: any, Class: new () => T) {
   return plainToClass(Class, plainObject);
 }
