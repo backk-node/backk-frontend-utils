d8197fa760c72a432044db172d2b9a733b6a6607
Petri Silen <petri.silen@silensoft.com>
2021-11-06 18:57:36 +0200

    Modified

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,86 @@
+import 'reflect-metadata';
+
 export { default as callRemoteService } from './callRemoteService';
 
+export { Values, Lengths, Durations } from './constants/constants';
 export { BackkError, PossibleBackkError } from './types/BackkError';
 export { PromiseErrorOr } from './types/PromiseErrorOr';
 export { One } from './types/One';
 export { Many } from './types/Many';
-export { default as DefaultPostQueryOperations } from './types/postqueryoperations/DefaultPostQueryOperations';
+
+export { default as ArrayNotUnique } from './decorators/typeproperty/ArrayNotUnique';
+export { default as BooleanOrTinyInt } from './decorators/typeproperty/BooleanOrTinyInt';
+export { default as IsAnyString } from './decorators/typeproperty/IsAnyString';
+export { default as IsBigInt } from './decorators/typeproperty/IsBigInt';
+export { default as IsCreditCardExpiration } from './decorators/typeproperty/IsCreditCardExpiration';
+export { default as IsCreditCardVerificationCode } from './decorators/typeproperty/IsCreditCardVerificationCode';
+export { default as IsDataUri } from './decorators/typeproperty/IsDataUri';
+export { default as IsFloat } from './decorators/typeproperty/IsFloat';
+export { default as IsNoneOf } from './decorators/typeproperty/IsNoneOf';
+export { default as IsOneOf } from './decorators/typeproperty/IsOneOf';
+export { default as IsPostalCode } from './decorators/typeproperty/IsPostalCode';
+export { default as IsStringOrObjectId } from './decorators/typeproperty/IsStringOrObjectId';
+export { default as IsSubject } from './decorators/typeproperty/IsSubject';
+export { default as IsUndefined } from './decorators/typeproperty/IsUndefined';
+export { default as LengthAndMatches } from './decorators/typeproperty/LengthAndMatches';
+export { default as LengthAndMatchesAll } from './decorators/typeproperty/LengthAndMatchesAll';
+export { default as MaxLengthAndMatches } from './decorators/typeproperty/MaxLengthAndMatches';
+export { default as MaxLengthAndMatchesAll } from './decorators/typeproperty/MaxLengthAndMatchesAll';
+export { default as MinMax } from './decorators/typeproperty/MinMax';
+export { default as ShouldBeTrue } from './decorators/typeproperty/ShouldBeTrue';
+export { default as ShouldBeTrueForObject } from './decorators/typeproperty/ShouldBeTrueForObject';
+
+// Root entity base classes
+export { default as Captcha } from './types/Captcha';
+export { default as Version } from './types/Version';
 export { default as _Id } from './types/_id/_Id';
-export { default as convertPlainToInstance } from './validation/convertPlainToInstance';
+export { default as _IdAndCaptcha } from './types/_id/_IdAndCaptcha';
+export { default as _IdAndCaptchaAndCreatedAtTimestamp } from './types/_id/_IdAndCaptchaAndCreatedAtTimestamp';
+export { default as _IdAndCaptchaAndCreatedAtTimestampAndLastModifiedTimestamp } from './types/_id/_IdAndCaptchaAndCreatedAtTimestampAndLastModifiedTimestamp';
+export { default as _IdAndCaptchaAndLastModifiedTimestamp } from './types/_id/_IdAndCaptchaAndLastModifiedTimestamp';
+export { default as _IdAndCaptchaAndVersion } from './types/_id/_IdAndCaptchaAndVersion';
+export { default as _IdAndCaptchaAndVersionAndCreatedAtTimestamp } from './types/_id/_IdAndCaptchaAndVersionAndCreatedAtTimestamp';
+export { default as _IdAndCaptchaAndVersionAndCreatedAtTimestampAndLastModifiedTimestamp } from './types/_id/_IdAndCaptchaAndVersionAndCreatedAtTimestampAndLastModifiedTimestamp';
+export { default as _IdAndCaptchaAndVersionAndLastModifiedTimestamp } from './types/_id/_IdAndCaptchaAndVersionAndLastModifiedTimestamp';
+export { default as _IdAndCreatedAtTimestamp } from './types/_id/_IdAndCreatedAtTimestamp';
+export { default as _IdAndCreatedAtTimestampAndLastModifiedTimestamp } from './types/_id/_IdAndCreatedAtTimestampAndLastModifiedTimestamp';
+export { default as _IdAndCreatedAtTimestampAndLastModifiedTimestampAndUserAccountId } from './types/_id/_IdAndCreatedAtTimestampAndLastModifiedTimestampAndUserAccountId';
+export { default as _IdAndCreatedAtTimestampAndUserAccountId } from './types/_id/_IdAndCreatedAtTimestampAndUserAccountId';
+export { default as _IdAndLastModifiedTimestamp } from './types/_id/_IdAndLastModifiedTimestamp';
+export { default as _IdAndLastModifiedTimestampAndUserAccountId } from './types/_id/_IdAndLastModifiedTimestampAndUserAccountId';
+export { default as _IdAndUserAccountId } from './types/_id/_IdAndUserAccountId';
+export { default as _IdAndVersion } from './types/_id/_IdAndVersion';
+export { default as _IdAndVersionAndCreatedAtTimestamp } from './types/_id/_IdAndVersionAndCreatedAtTimestamp';
+export { default as _IdAndVersionAndCreatedAtTimestampAndLastModifiedTimestamp } from './types/_id/_IdAndVersionAndCreatedAtTimestampAndLastModifiedTimestamp';
+export { default as _IdAndVersionAndCreatedAtTimestampAndLastModifiedTimestampAndUserAccountId } from './types/_id/_IdAndVersionAndCreatedAtTimestampAndLastModifiedTimestampAndUserAccountId';
+export { default as _IdAndVersionAndCreatedAtTimestampAndUserAccountId } from './types/_id/_IdAndVersionAndCreatedAtTimestampAndUserAccountId';
+export { default as _IdAndVersionAndLastModifiedTimestamp } from './types/_id/_IdAndVersionAndLastModifiedTimestamp';
+export { default as _IdAndVersionAndLastModifiedTimestampAndUserAccountId } from './types/_id/_IdAndVersionAndLastModifiedTimestampAndUserAccountId';
+export { default as _IdAndVersionAndUserAccountId } from './types/_id/_IdAndVersionAndUserAccountId';
+
+export { default as Id } from './types/id/Id';
+
+// PostQueryOperations
+export { default as _IdAndDefaultPostQueryOperations } from './types/postqueryoperations/_IdAndDefaultPostQueryOperations';
+export { default as _IdsAndDefaultPostQueryOperations } from './types/postqueryoperations/_IdsAndDefaultPostQueryOperations';
+export { default as DefaultPagination } from './types/postqueryoperations/DefaultPagination';
+export { default as DefaultPostQueryOperations } from './types/postqueryoperations/DefaultPostQueryOperations';
+export { default as DefaultSorting } from './types/postqueryoperations/DefaultSorting';
+export { default as DefaultSortingAndPagination } from './types/postqueryoperations/DefaultSortingAndPagination';
+export { default as Pagination } from './types/postqueryoperations/Pagination';
+export { PostQueryOperations } from './types/postqueryoperations/PostQueryOperations';
+export { Projection } from './types/postqueryoperations/Projection';
+export { default as SortBy } from './types/postqueryoperations/SortBy';
+export { SortBys } from './types/postqueryoperations/SortBys';
+export { default as OrFilter } from './types/userdefinedfilters/OrFilter';
+export { default as UserDefinedFilter } from './types/userdefinedfilters/UserDefinedFilter';
+export { default as EntityCountRequest } from './types/EntityCountRequest';
+
+// User account base entity
+export { default as BaseUserAccount } from './types/useraccount/BaseUserAccount';
+export { default as Issuer } from './types/useraccount/Issuer';
+export { default as UserAccountId } from './types/useraccount/UserAccountId';
+export { default as Subject } from './types/useraccount/Subject';
 
 export * from 'class-validator';
+export { Type } from 'class-transformer';
--- src/validation/convertPlainToInstance.ts
+++
@@ -1,5 +0,0 @@
-import { plainToClass } from 'class-transformer';
-
-export default function convertPlainToInstance<T>(plainObject: any, Class: new () => T) {
-  return plainToClass(Class, plainObject);
-}
