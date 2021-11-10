ee0b7ee1869f6caa04f22f201103a5340239882f
Petri Silen <petri.silen@silensoft.com>
2021-11-10 14:57:48 +0200

    Modified

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 
 export { default as callRemoteService } from './callRemoteService';
-export { default as validateServiceFunctionArgumentOrThrow } from './validation/validateServiceFunctionArgument';
+export { default as validateServiceFunctionArgument } from './validation/validateServiceFunctionArgument';
 export { default as useState } from './useState';
 
 export { Values, Lengths, Durations } from './constants/constants';
