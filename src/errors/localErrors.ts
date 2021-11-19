23638f0319d7b8552d3e4c2a002388801faec18f
Petri Silen <petri.silen@silensoft.com>
2021-11-19 14:27:50 +0200

    Modified

--- src/errors/localErrors.ts
+++ src/errors/localErrors.ts
@@ -1,11 +1,11 @@
 export const localErrors = {
   ACCESS_TOKEN_ENCRYPTION_KEY_NOT_SET: {
-    errorCode: 'backkLocalError.1',
+    errorCode: 'localError.1',
     message:
       "Access token storage encryption key is not set. Use 'EncryptionKeyManager.setAccessTokenStorageEncryptionKey()' function to set the encryption key for services before using them",
   },
   NETWORK_ERROR: {
-    errorCode: 'backkLocalError.2',
+    errorCode: 'localError.2',
     message: 'Remote service call failed due to network error',
   },
 };
