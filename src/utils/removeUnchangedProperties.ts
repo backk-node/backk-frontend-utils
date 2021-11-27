53ad3a802b4fe6b56e1c46923c626cee22afbc6d
Petri Silen <petri.silen@silensoft.com>
2021-11-27 11:14:28 +0200

    Modified

--- src/utils/removeUnchangedProperties.ts
+++ src/utils/removeUnchangedProperties.ts
@@ -1,9 +1,14 @@
+import isEqual from 'lodash/isEqual';
+
 export default function removeUnchangedProperties<T extends { [key: string]: any }>(
   newInstance: T,
   currentInstance: T
 ) {
   Object.keys(newInstance).forEach((propertyName) => {
-    if (newInstance[propertyName] === '' || newInstance[propertyName] === currentInstance[propertyName]) {
+    if (
+      newInstance[propertyName] === '' ||
+      isEqual(newInstance[propertyName], currentInstance[propertyName])
+    ) {
       delete newInstance[propertyName];
     }
   });
